bc078d5b96d5eb92c3f7f9fb21d6eeb392180483
Eugenio Minervi <eugenio.minervi@gmail.com>
2019-09-19 16:15:43 +0200

    First commit for JobLab Project: - WebContent/home.html (Project Home) - it/acn/joblab/servlets/LoginServlet.java (Servlet for Login Feature)

---
+++ src/it/acn/joblab/servlets/LoginServlet.java
@@ -0,0 +1,49 @@
+package it.acn.joblab.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doAction(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println(request.getParameter("username")+" "+request.getParameter("password"));
+	}
+
+}
